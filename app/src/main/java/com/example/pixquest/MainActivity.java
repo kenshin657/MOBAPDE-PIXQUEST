7e5a563930f212feb216387d3af745e862415ce1
Kyle T <kyle_tagle@dlsu.edu.ph>
2019-11-22 23:18:47 +0800

    google sign in skeleton

--- app/src/main/java/com/example/pixquest/MainActivity.java
+++ app/src/main/java/com/example/pixquest/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.pixquest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
     }
+
+
 }
---
+++ app/src/main/java/com/example/pixquest/PixQuest_Main.java
@@ -0,0 +1,14 @@
+package com.example.pixquest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PixQuest_Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pix_quest__main);
+    }
+}
