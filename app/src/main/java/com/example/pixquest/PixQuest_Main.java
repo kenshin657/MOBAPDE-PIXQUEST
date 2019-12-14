67bf8bdd278c965cd797ef1cae6951fd006d4549
DLSUSanti <santiago_alvarez@dlsu.edu.ph>
2019-12-14 21:58:53 +0800

    text color

--- app/src/main/java/com/example/pixquest/PixQuest_Main.java
+++ app/src/main/java/com/example/pixquest/PixQuest_Main.java
@@ -14,7 +14,6 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.Toast;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
